867369a4d639004152e29999a62736d9cc12f945
Sean Lee <freshdried@gmail.com>
2013-04-18 18:23:36 -0400

    updated broadcast

--- broadcast.py
+++ broadcast.py
@@ -5,17 +5,31 @@ from random import choice
 ESPEAK_CMD = "ssh osh.it.cx 'espeak --stdout|aplay -t wav -'"
 NAME = "Sean"
 
+# Populate tasks
 tasks = Popen(["t --list"], shell=True, stdout=PIPE).communicate()[0].decode('utf-8').split('\n')[:-1]
 
-def say(s):
-	p = Popen([ESPEAK_CMD], shell=True, stdin=PIPE)
-	p.communicate(input=bytes(s, 'utf-8'))
+# Expand tasks
+def expand(task):
+	mydict = {
+			'hw': 'homework',
+			'520': 'multivariable',
+			}
+	for k in mydict.keys():
+		task = task.replace(k, mydict[k]);
+[expand(task) for task in tasks]
 
-#Generate Sentence
+
+# Sentence blocks
 greeting = [
 		'Hey ' + NAME + '!',
 		NAME + ',',
 		'Yo ' + NAME + '!', 
+		]
+idle = [
+		'You should populate your todo list!',
+		'What should you really be doing right now?',
+		'Are you really done with your tasks for today?',
+		'Add stuff to your todo list!',
 		]
 ask = [
 		lambda x: 'Did you ' + x + ' yet?',
@@ -27,19 +41,33 @@ dontforget = [
 		"Also, Don't forget to",
 		]
 
-out = []
-out.append(choice(greeting))
-out.append(choice(ask)(tasks[0]))
-if len(tasks) > 2:
-	out.append(choice(dontforget))
-	[out.append(task + ',') for task in tasks[1:-2]]
-	out.append(tasks[-2] + ', and')
-	out.append(tasks[-1] + '.')
-elif len(tasks) > 1:
-	out.append(choice(dontforget))
-	out.append(tasks[-1] + '.')
+# Generate sentence
+def generate(tasks):
+	out = []
+	if len(tasks) == 0:
+		out.append(choice(greeting))
+		out.append(choice(idle))
+	elif len(tasks) == 1:
+		out.append(choice(greeting))
+		out.append(choice(ask)(tasks[0]))
+	elif len(tasks) == 2:
+		out.append(choice(greeting))
+		out.append(choice(ask)(tasks[0]))
+		out.append(choice(dontforget))
+		out.append(tasks[-1] + '.')
+	if len(tasks) > 2:
+		out.append(choice(greeting))
+		out.append(choice(ask)(tasks[0]))
+		out.append(choice(dontforget))
+		[out.append(task + ',') for task in tasks[1:-2]]
+		out.append(tasks[-2] + ', and')
+		out.append(tasks[-1] + '.')
+	return ' '.join(out)
 
+generated = generate(tasks)
 
-output = ' '.join(out)
-print(output)
-say(output)
+# Say Sentence
+def say(s):
+	p = Popen([ESPEAK_CMD], shell=True, stdin=PIPE)
+	p.communicate(input=bytes(s, 'utf-8'))
+say(generated)
