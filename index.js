a810e5d2ceeb3710dc8357455db634906f56e33e
Sean Lee <freshdried@gmail.com>
2014-03-16 14:53:24 -0400

    ported to javascript

---
+++ index.js
@@ -0,0 +1,71 @@
+exports.parse = function(lines, populate, marker) {
+	var populate = populate || function(obj) { return obj;};
+	var marker = marker || '\t';
+
+	var TreeNode = function(data) {
+		this.data = data;
+		this.parent = null;
+		this.children = [];
+	}
+	var tree = new TreeNode({
+		'type': 'root',
+	    	'depth': -1
+	});
+	var levels = [tree];
+
+	for (var i = 0; i < lines.length; i++) {
+		var line = lines[i];
+		var hascontent = false;
+		var tabcount = 0;
+
+
+		for (var j = 0; j < line.length; j++) {
+			var ch = line[j];;
+			if ((ch == '\t')) {
+				tabcount += 1;
+			}else if (/[^\s]/.test(ch)){
+				hascontent = true;
+				break;
+			}
+		}
+		if (hascontent) { //then add node to tree
+			var content = {
+				'line': line.substring(tabcount),
+				'type': "line",
+			};
+			//populate later (with depth also)
+
+			var node = new TreeNode(content);
+			node.tabcount = tabcount;
+
+			function topnode() {
+		       		return levels[levels.length - 1];
+			}
+			while(node.tabcount - topnode().tabcount <= 0) {
+				levels.pop();
+			}
+			node.data.depth = levels.length - 1;
+			node = populate(node);
+			node.parent = topnode();
+			node.parent.children.push(node);
+			levels.push(node);
+		}
+	}
+	return tree;
+}
+exports.print = function (tree){
+	function _print(node) {
+		if (node.data.type !== 'root') {
+			var string = "";
+			for (var i = 0; i < node.data.depth; i++) {
+				string += "\t";
+			}
+			string += node.data.line;
+			console.log(string);
+		}
+		for (var i = 0; i < node.children.length; i++) {
+			_print(node.children[i]);
+		}
+	}
+	_print(tree);
+}
